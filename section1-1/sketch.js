470643fad9e22d49f656d5c2684a4a1488293d9f
Akira2840 <1840835h@sty.kobe-u.ac.jp>
2020-10-13 12:56:09 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(178, 111, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
